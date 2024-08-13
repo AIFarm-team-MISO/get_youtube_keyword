2e89d690ec8bf779f089ec2df2a7f18fcccce46a
Chinho Choi <sevenstar15@naver.com>
2024-08-13 21:43:21 +0900

    ignore api

--- tool/make_youtube_api.py
+++ tool/make_youtube_api.py
@@ -2,7 +2,11 @@ from googleapiclient.discovery import build
 
 # Google API 클라이언트를 빌드합니다.
 
+import os
+
+#api_key = os.getenv('API_KEY')
 #api_key = 'AIzaSyDZfPV3PIFTZJCEWB69OuL4dKHq7zENp9c'
 api_key = 'AIzaSyDOX3lrgtjw5ClyWTG2wmWTZwwtfvuHbXo'
+
 def build_youtube_api():
     return build('youtube', 'v3', developerKey=api_key)
